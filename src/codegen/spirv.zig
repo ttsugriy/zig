891e492506a088680d91a6059675a88bfeff0788
Andrew Kelley <andrew@ziglang.org>
2023-05-31 15:00:48 -0700

    InternPool: remove memoized_decl
    This is neither a type nor a value. Simplifies `addStrLit` as well as
    the many places that switch on `InternPool.Key`.
    
    This is a partial revert of bec29b9e498e08202679aa29a45dab2a06a69a1e.

--- src/codegen/spirv.zig
+++ src/codegen/spirv.zig
@@ -864,9 +864,7 @@ pub const DeclGen = struct {
 
                     try self.addUndef(layout.padding);
                 },
-                .memoized_decl,
-                .memoized_call,
-                => unreachable,
+                .memoized_call => unreachable,
             }
         }
     };
